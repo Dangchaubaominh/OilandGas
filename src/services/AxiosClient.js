0cde862d9996e57e527d02ac27cc8703560793e2
Đặng Châu Bảo Minh <dangchaubaominh2023@gmail.com>
2026-03-12 13:55:38 +0700

    Change URL

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  // baseURL: "http://localhost:3000/api", 
-  baseURL: "https://oil-gas-omega.vercel.app/api",
+  baseURL: "http://localhost:3000/api", 
   headers: { "Content-Type": "application/json" },
 });
 
